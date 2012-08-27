7c3d231b1c5aecdada1fa4bb5c745f453ebcaebf
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-27 08:04:42 +0000

    git-svn-id: https://plugins.svn.wordpress.org/widget-manager-light/tags/1.0@590816 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- include/otw_sidebar_options.php
+++ include/otw_sidebar_options.php
@@ -14,10 +14,10 @@ if( isset( $_GET['message'] ) && isset( $messages[ $_GET['message'] ] ) ){
 	$message .= $messages[ $_GET['message'] ];
 }
 ?>
-<div class="updated"><p>
+
 <div class="updated"><p>Check out the <a href="http://otwthemes.com/online-documentation-widget-manager-light/?utm_source=wp.org&utm_medium=admin&utm_content=docs&utm_campaign=wml">Online documentation</a> for this plugin<br /><br /> Upgrade to the full version of <a href="http://otwthemes.com/product/sidebar-widget-manager-for-wordpress/?utm_source=wp.org&utm_medium=admin&utm_content=upgrade&utm_campaign=wml">Sidebar and Widget Manager</a> | <a href="http://otwthemes.com/demos/sidebar-widget-manager/?utm_source=wp.org&utm_medium=admin&utm_content=upgrade&utm_campaign=wml">Demo site</a> | <a href="http://www.youtube.com/watch?v=WT9UK1eX4C8">Video overview</a><br /><br />
 Follow on <a href="http://twitter.com/OTWthemes">Twitter</a> | <a href="http://www.facebook.com/pages/OTWthemes/250294028325665">Facebook</a> | <a href="http://www.youtube.com/OTWthemes">YouTube</a> | <a href="https://plus.google.com/117222060323479158835/about">Google+</a></p></div>
-</p></div>
+
 <?php if ( $message ) : ?>
 <div id="message" class="updated"><p><?php echo $message; ?></p></div>
 <?php endif; ?>
