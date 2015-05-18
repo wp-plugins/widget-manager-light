46be8f8553729472931296f3e0731a1d3cac00d7
OTWthemes <OTWthemes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-18 13:30:09 +0000

    1.11 - Fixed: Visibility tabs posts pagination
    git-svn-id: https://plugins.svn.wordpress.org/widget-manager-light/tags/1.11@1162679 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ include/otw_sidebar_info.php
@@ -0,0 +1,20 @@
+<?php
+/** Plugin Information
+  *
+  */
+?>
+<div class="wrap">
+	<div id="icon-edit" class="icon32"><br/></div>
+	<h2>
+		<?php _e('Info') ?>
+	</h2>
+	<div class="form-wrap" id="poststuff">
+		<div id="post-body">
+				<div id="post-body-content">
+					<div class="form-field">
+						<p><?php _e( 'Check out the <a href="http://otwthemes.com/online-documentation-widget-manager-light/?utm_source=wp.org&utm_medium=admin&utm_content=docs&utm_campaign=wml">Online documentation</a> for this plugin<br /><br /> Upgrade to the full version of <a href="http://codecanyon.net/item/sidebar-widget-manager-for-wordpress/2287447?ref=OTWthemes">Sidebar and Widget Manager</a> | <a href="http://otwthemes.com/demos/1ts/?item=Sidebar%20Widget%20Manager&utm_source=wp.org&utm_medium=admin&utm_content=upgrade&utm_campaign=wml">Demo site</a>  <br /><br /> <a href="http://otwthemes.com/widgetizing-pages-in-wordpress-can-be-even-easier-and-faster?utm_source=wp.org&utm_medium=admin&utm_content=site&utm_campaign=wml">Create responsive layouts in minutes, drag & drop interface, feature rich.</a>' );?></p>
+					</div>
+				</div>
+		</div>
+	</div>
+</div>
